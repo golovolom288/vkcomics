6736c0b336c54730a57c43282f42ded2b499e350
golovolom288 <142310490+golovolom288@users.noreply.github.com>
2024-09-18 20:25:30 +0600

    лишние действия из функций пересены в main, постоянная отправка комикса в тг удалена

--- save_img.py
+++ save_img.py
@@ -1,10 +1,9 @@
-import requests, os
+import requests
 
 
 def save_img(url, filepath):
     response = requests.get(url)
     response.raise_for_status()
-    os.makedirs("comics", exist_ok=True)
     with open(filepath, 'wb') as file:
         file.write(response.content)
 
--- tg_bot.py
+++ tg_bot.py
@@ -1,29 +1,18 @@
-import shutil
-
 from dotenv import load_dotenv
 import telegram
 import os
-import time
-import random
 from get_comics import get_comics
 
 
-def send_posts_tg_bot(hours, tg_bot_token, chat_id, alt, directory="images/"):
-    photos = os.listdir(directory)
-    random.shuffle(photos)
+def send_posts_tg_bot(tg_bot_token, chat_id, alt, directory="comics/"):
     bot = telegram.Bot(token=tg_bot_token)
-    while True:
-        for photo in photos:
-            photo_path = os.path.join(directory, photo)
-            with open(photo_path, "rb") as photo:
-                bot.send_photo(chat_id=chat_id, photo=photo, caption=alt)
-            shutil.rmtree("comics")
-            time.sleep(int(hours*3600))
+    photo_path = os.path.join(directory, "comics.jpg")
+    with open(photo_path, "rb") as photo:
+        bot.send_photo(chat_id=chat_id, photo=photo, caption=alt)
 
 
 if __name__ == "__main__":
     load_dotenv()
     chat_id = os.environ["TG_GROUP_ID"]
-    hours = int(os.environ["TG_TIME"])
     tg_bot_token = os.environ["TG_BOT_TOKEN"]
-    send_posts_tg_bot(hours, tg_bot_token, chat_id, get_comics()[1], directory="comics/")
+    send_posts_tg_bot(tg_bot_token, chat_id, get_comics()[1], directory="comics/")
