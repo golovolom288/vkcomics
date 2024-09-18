2821ee583c1839a29592eee02ac4f3b8a0b3320e
golovolom288 <142310490+golovolom288@users.noreply.github.com>
2024-09-18 20:37:19 +0600

    все файлы совмещены в единое целое, за ненадобностью

--- vkcomics.py
+++ vkcomics.py
@@ -1,15 +1,41 @@
 import os
-from save_img import save_img
 from dotenv import load_dotenv
-from tg_bot import send_posts_tg_bot
-from get_comics import get_comics
+import telegram
+import random
 import shutil
+import requests
 
 
-load_dotenv()
-tg_bot_token = os.environ["TG_BOT_TOKEN"]
-chat_id = os.environ["tg_group_id"]
-os.makedirs("comics", exist_ok=True)
-save_img(get_comics()[0], "comics/comics.jpg")
-send_posts_tg_bot(tg_bot_token, chat_id, get_comics()[1], directory="comics/")
-shutil.rmtree("comics")
+def get_comics():
+    comics_number = random.randrange(1, 2984, 1)
+    site_url = f"https://xkcd.com/{comics_number}/info.0.json"
+    site_response = requests.get(site_url)
+    site_response.raise_for_status()
+    site_json = site_response.json()
+    comics_comment = site_json["alt"]
+    comics_url = site_json["img"]
+    return comics_url, comics_comment
+
+
+def save_img(url, filepath):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+
+
+def send_posts_tg_bot(tg_bot_token, chat_id, alt, directory="comics/"):
+    bot = telegram.Bot(token=tg_bot_token)
+    photo_path = os.path.join(directory, "comics.jpg")
+    with open(photo_path, "rb") as photo:
+        bot.send_photo(chat_id=chat_id, photo=photo, caption=alt)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    tg_bot_token = os.environ["TG_BOT_TOKEN"]
+    chat_id = os.environ["tg_group_id"]
+    os.makedirs("comics", exist_ok=True)
+    save_img(get_comics()[0], "comics/comics.jpg")
+    send_posts_tg_bot(tg_bot_token, chat_id, get_comics()[1], directory="comics/")
+    shutil.rmtree("comics")
